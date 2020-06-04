cd0a41be8f91c5e82e4adcd21666cf58e3e51459
Rodrigo Barona <rbarona@gmail.com>
2020-06-04 12:04:29 +0100

    add name tag for anchor

--- src/components/SectionReviews.js
+++ src/components/SectionReviews.js
@@ -7,7 +7,7 @@ export default class SectionReviews extends React.Component {
     render() {
         let section = _.get(this.props, 'section');
         return (
-            <section id={_.get(section, 'section_id')} className={'block reviews-block bg-' + _.get(section, 'background') + ' outer'}>
+            <section id={_.get(section, 'section_id')} name={_.get(section, 'section_id')} className={'block reviews-block bg-' + _.get(section, 'background') + ' outer'}>
               <div className="block-header inner-small">
                 {_.get(section, 'title') && 
                 <h2 className="block-title">{_.get(section, 'title')}</h2>
