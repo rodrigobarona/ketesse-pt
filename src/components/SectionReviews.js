fb0f6e4336d9e430851b0720a347fa2b35f09340
Rodrigo Barona <rbarona@gmail.com>
2020-06-05 15:01:56 +0100

    Change cite for H2 on Author

--- src/components/SectionReviews.js
+++ src/components/SectionReviews.js
@@ -28,7 +28,7 @@ export default class SectionReviews extends React.Component {
                         <img className="review-avatar" src={safePrefix(_.get(review, 'avatar'))} alt="Author avatar"/>
                         }
                       <footer className="review-footer">
-                        <cite className="review-author">{_.get(review, 'tipo_dor')}</cite>
+                        <h2 className="review-author">{_.get(review, 'tipo_dor')}</h2>
                       </footer>
                     </div>
                   </blockquote>
