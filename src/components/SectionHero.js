8984c2e68615eb439e769a6cff9feebd6a8240a0
Rodrigo Barona <rbarona@gmail.com>
2020-06-05 10:07:45 +0100

    add lazyload to img

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -13,7 +13,7 @@ export default class SectionHero extends React.Component {
                 <div className="grid">
                   {_.get(section, 'image') && 
                   <div className="cell block-preview">
-                    <img src={safePrefix(_.get(section, 'image'))}? alt={_.get(section, 'title')} />
+                    <img src={safePrefix(_.get(section, 'image'))}? alt={_.get(section, 'title')} loading="lazy" />
                   </div>
                   }
                   
