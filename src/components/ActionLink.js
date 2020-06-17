23862c91791c82d9aedeab71c003877bc3f4f1ee
Rodrigo Barona <rbarona@gmail.com>
2020-06-17 23:44:39 +0100

    add achor link to action buttons

--- src/components/ActionLink.js
+++ src/components/ActionLink.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
+import { AnchorLink } from "gatsby-plugin-anchor-links";
 
 import {Link, safePrefix} from '../utils';
 
@@ -8,9 +9,9 @@ export default class ActionLink extends React.Component {
         let action = _.get(this.props, 'action');
         let class_names = _.get(this.props, 'class_names');
         return (
-            <Link to={safePrefix(_.get(action, 'url'))}
+            <AnchorLink to={safePrefix(_.get(action, 'url'))}
                {...(_.get(action, 'new_window') ? {target: '_blank', rel: 'noopener'} : null)}
-               className={class_names}>{_.get(action, 'label')}</Link>
+               className={class_names}>{_.get(action, 'label')}</AnchorLink>
         );
     }
 }
