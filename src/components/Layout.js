28264b1bd02b9595440225feaae8f3c2c294679b
Rodrigo Barona <rbarona@gmail.com>
2020-06-17 13:00:50 +0100

    change call GTM

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -19,11 +19,18 @@ export default class Body extends React.Component {
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
 
                     
-                      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-NFVBF7S');</script>
+                    
+
+                         <script async src={`https://www.googletagmanager.com/gtag/js?id=GTM-NFVBF7S`}></script>
+                          <script>
+                            {`
+                              window.dataLayer = window.dataLayer || [];
+                              function gtag(){dataLayer.push(arguments);}
+                              gtag('js', new Date());
+                              gtag('config', GTM-NFVBF7S);
+                            `}
+                          </script>
+
                 </Helmet>
 
                 
