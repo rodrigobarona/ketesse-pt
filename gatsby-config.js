cc99880c4a9f0b70eedb81a02544f0df9da3b329
Rodrigo Barona <rbarona@gmail.com>
2020-06-17 13:18:39 +0100

    Add gatsby-plugin-google-tagmanager

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,33 @@ module.exports = {
             }
         },
         {
+            resolve: "gatsby-plugin-google-tagmanager",
+            options: {
+              id: "GTM-NFVBF7S",
+
+              // Include GTM in development.
+              //
+              // Defaults to false meaning GTM will only be loaded in production.
+              includeInDevelopment: false,
+
+              // datalayer to be set before GTM is loaded
+              // should be an object or a function that is executed in the browser
+              //
+              // Defaults to null
+              defaultDataLayer: { platform: "gatsby" },
+
+              // Specify optional GTM environment details.
+              gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+              gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+              dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+              // Name of the event that is triggered
+              // on every Gatsby route change.
+              //
+              // Defaults to gatsby-route-change
+              routeChangeEventName: "website-change-page",
+        },
+        {
             resolve: `gatsby-plugin-scroll-reveal`,
             options: {
                 
