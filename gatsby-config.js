c45012582454d83017a5107d2c70b5eb114fb630
Rodrigo Barona <rbarona@gmail.com>
2020-06-17 15:01:17 +0100

    uptade " to ` and delete comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,32 +31,13 @@ module.exports = {
             }
         },
         {
-            resolve: "gatsby-plugin-google-tagmanager",
+            resolve: `gatsby-plugin-google-tagmanager`,
             options: {
-              id: "GTM-NFVBF7S",
-
-              // Include GTM in development.
-              //
-              // Defaults to false meaning GTM will only be loaded in production.
+              id: `GTM-NFVBF7S`,
               includeInDevelopment: false,
-
-              // datalayer to be set before GTM is loaded
-              // should be an object or a function that is executed in the browser
-              //
-              // Defaults to null
-              defaultDataLayer: { platform: "gatsby" },
-
-              // Specify optional GTM environment details.
-              //gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-              //gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-              //dataLayerName: "YOUR_DATA_LAYER_NAME",
-
-              // Name of the event that is triggered
-              // on every Gatsby route change.
-              //
-              // Defaults to gatsby-route-change
-              routeChangeEventName: "website-change-page",
-            },
+              defaultDataLayer: { platform: `gatsby` },
+              routeChangeEventName: `website-change-page`,
+            }
         },
         {
             resolve: `gatsby-plugin-scroll-reveal`,
