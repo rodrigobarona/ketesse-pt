4195cd75a19b6d14de577ba7cf43dfc4c94e36c4
Rodrigo Barona <rbarona@gmail.com>
2020-06-17 12:31:35 +0100

    Audit Fix + Google Tag Manager

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -17,7 +17,18 @@ export default class Body extends React.Component {
                     <meta name="google" content="notranslate" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400,400i,700,700i&display=swap" rel="stylesheet"/>
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
+
+                    {/* Global site tag (gtag.js) - Google Analytics  */}
+                      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-NFVBF7S');</script>
                 </Helmet>
+                
+                {/* Global site tag (gtag.js) - Google Analytics  */}
+                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NFVBF7S" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
                 <div id="page" className={'site palette-' + _.get(this.props, 'pageContext.site.siteMetadata.palette')}>
                   <Header {...this.props} />
                   <main id="content" className="site-content">
