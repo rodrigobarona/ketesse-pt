b31ed11fb1f5ee235991023e5e67037485bee616
Rodrigo Barona <rbarona@gmail.com>
2020-06-19 01:17:37 +0100

    making large sutitle

--- src/components/SectionFeatures.js
+++ src/components/SectionFeatures.js
@@ -9,7 +9,7 @@ export default class SectionFeatures extends React.Component {
         let section = _.get(this.props, 'section');
         return (
             <section id={_.get(section, 'section_id')}  name={_.get(section, 'section_id')} className={'block features-block bg-' + _.get(section, 'background') + ' outer'}>
-              <div className="block-header inner-small">
+              <div className="block-header inner-large">
                 {_.get(section, 'title') && 
                 <h2 className="block-title">{_.get(section, 'title')}</h2>
                 }
