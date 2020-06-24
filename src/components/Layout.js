567cf26947fcaae5beb5aa1fd23f3b97a2e54d57
Rodrigo Barona <rbarona@gmail.com>
2020-06-24 01:51:17 +0100

    add site description

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,6 +13,7 @@ export default class Body extends React.Component {
                 <Helmet>
                     <title>{_.get(this.props, 'pageContext.frontmatter.title') && _.get(this.props, 'pageContext.frontmatter.title') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.title')}</title>
                     <meta charSet="utf-8"/>
+                    <meta name="description" content={_.get(this.props, 'pageContext.site.siteMetadata.site_description')} />
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                     <meta name="google" content="notranslate" />
                     <link rel="preload" rel="stylesheet" href={safePrefix('assets/css/main.css')} />
