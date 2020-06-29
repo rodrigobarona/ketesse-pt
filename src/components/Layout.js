ed0eb58363437a8b046fd0ddfe6fa4d1db0233a3
Rodrigo Barona <rbarona@gmail.com>
2020-06-29 16:21:11 +0100

    add OneTrust script

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,12 +11,14 @@ export default class Body extends React.Component {
         return (
             <React.Fragment>
                 <Helmet>
+                    <script src="/onetrust/ketesse.js"></script>
                     <title>{_.get(this.props, 'pageContext.frontmatter.title') && _.get(this.props, 'pageContext.frontmatter.title') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.title')}</title>
                     <meta charSet="utf-8"/>
                     <meta name="description" content={_.get(this.props, 'pageContext.site.siteMetadata.site_description')} />
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                     <meta name="google" content="notranslate" />
                     <link rel="preload" rel="stylesheet" href={safePrefix('assets/css/main.css')} />
+                    <link rel="preload" rel="stylesheet" href={safePrefix('/onetrust/ketesse.css')} />
                 </Helmet>
                 <div id="page" className={'site palette-' + _.get(this.props, 'pageContext.site.siteMetadata.palette')}>
                   <Header {...this.props} />
