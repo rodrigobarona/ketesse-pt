4d91981b6f781b35d523488579607396f7a1e94f
Rodrigo Barona <rbarona@gmail.com>
2020-06-30 02:01:52 +0100

    Revert "change script to link tag"
    This reverts commit a62fbe10a2e484f65063e598f093e40a39e2791f.

--- src/html.js
+++ src/html.js
@@ -5,7 +5,7 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
-        <link as="script" rel="preload" href='/onetrust/ketesse.js'>
+        <script src='/onetrust/ketesse.js'></script>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
